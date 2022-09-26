90847ce5bc8cedb2dbbc940036be5445b008a915
yasinabdmahmood <abd181103@gmail.com>
2022-09-26 17:10:50 +0300

    Implement duck pattern in catagories.js file

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,1 +1,15 @@
+export const CHECK_STATUS = 'CHECK_STATUS';
 
+const initialState = [];
+
+export default statusReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CHECK_STATUS:
+      return 'Under Construction';
+    default: return state;
+  }
+};
+
+export const checkStatus = () => ({
+  type: CHECK_STATUS,
+});
