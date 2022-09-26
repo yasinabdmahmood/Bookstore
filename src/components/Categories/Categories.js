34dc5ac26f46da6307fb78e5c7f0483666815d66
yasinabdmahmood <abd181103@gmail.com>
2022-09-27 02:13:35 +0300

    Add dispatch functionality to Catagories component

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../../redux/categories/categories';
 
 function Categories() {
+  const dispatch = useDispatch();
   return (
     <div>
-      <button type="submit">Check status</button>
+      <button type="submit" onClick={() => { dispatch(checkStatus()); }}>
+        Check status
+      </button>
     </div>
   );
 }
